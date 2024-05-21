01b9b8024db40a04d133f3f148f9b819af873b53
Isaac Gabriel <codebiisaac@gmail.com>
2024-05-21 13:42:58 -0300

    Scroll Up

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,7 +63,12 @@ const sendEmail = (e) => {
 
 contactForm.addEventListener('submit', sendEmail)
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') 
+    : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
